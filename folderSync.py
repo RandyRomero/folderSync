744471b326805c336012fe4674b29d21f0898c7b
RandyRomero <a.ya.miheev@gmail.com>
2017-04-02 14:19:03 +0300

    add paths to folder to be synced, add config forlogging

--- folderSync.py
+++ folderSync.py
@@ -2,5 +2,16 @@
 
 '''Program that can sync all files and folders between two chosen folders. I need it to keep my photo-backup updated. But script should be able to sync in both ways. And keep track of changes in both folders.'''
 
-import os
+import logging, os
+
+logging.basicConfig(
+	format = "%(levelname) -1s %(asctime)s line %(lineno)s: %(message)s",
+	level = logging.DEBUG
+	)
+
+#TODO make menu to let user choose folders to sync
+# ubtil then I am gonna set it manually at code to save some time while checking
+
+firstFolder = ('.\\A')
+secondFolder = ('.\\B')
 
