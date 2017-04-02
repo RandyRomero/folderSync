313cbc4e0c3f21731503b86f0c599934ca04af24
RandyRomero <a.ya.miheev@gmail.com>
2017-04-02 12:06:46 +0300

    Initial commit. Make file 'folderSync.py' and .gitignore to ignore two folders for testing ignored by git

---
+++ folderSync.py
@@ -0,0 +1,6 @@
+#! python3
+
+'''Program that can sync all files and folders between two chosen folders. I need it to keep my photo-backup updated. But script should be able to sync in both ways. And keep track of changes in both folders.'''
+
+import os
+
